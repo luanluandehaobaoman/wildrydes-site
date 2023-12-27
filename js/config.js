1f564b6adce7b19cd25c478ca179a304766fbf5d
root <347036700@qq.com>
2023-12-27 16:33:23 +0800

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_SoPfmgvze', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '313nl66fb1lon3mf5o3hpk20ml', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
